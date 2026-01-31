88c4cbea81de129d22b8c2f540cb5f40ce9282cc
javicasper <javicasper@users.noreply.github.com>
2026-01-31 17:33:15 +0100

    fix: enable Carbon SCSS by using globals.scss with Sass loader

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import './globals.css';
+import './globals.scss';
 import { useState } from 'react';
 import { Content } from '@carbon/react';
 import ThemeProvider from '@/components/theme/ThemeProvider';
